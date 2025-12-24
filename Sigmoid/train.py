79850b16217d20cf3edf6ff4e3ed9f395918dcca
IjunaidI <d87816476@gmail.com>
2025-12-24 19:41:34 +0500

    added binary logistic regression and also add ploting

--- Sigmoid/train.py
+++ Sigmoid/train.py
@@ -1,13 +1,10 @@
-
-import numpy as np
 import pandas as pd
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 loss_plot = []
 epochs_plot = []
-
-
-class LinearRegressionModel:
+class SigmoidModel:
     def __init__(self,weights=[],bias=0):
         self.weights = weights
         self.bias = bias
@@ -34,40 +31,41 @@ class LinearRegressionModel:
        
     def predict(self,input):
                 product_w = self.weights * input
-                return product_w.sum() + self.bias
+                prediction = product_w.sum() + self.bias
+                prediction = 1/(math.e**(-prediction) + 1)
+                return prediction
     def gradient_descent(self,predictions,input,output,learning_rate):
-         n = len(predictions)
-         
-         _loss = 0
-         for i in range(0,self.col_amount):
-            series_of_loss = predictions - output
-            _loss = series_of_loss**2
-            
-            _loss = _loss.sum()
-            _loss = _loss/n
-           
-            series_of_derivatives = series_of_loss*input.iloc[:,i]
-            total_loss = (series_of_derivatives.sum()*2)/n
-            step = total_loss*learning_rate
-            new_weight = self.weights[i] - step
-            self.weights[i] = new_weight
-         return _loss
+            n = len(predictions)
+            predictions = pd.Series(predictions)
+            _loss = 0
+            series_of_difference = predictions - output
+            loss_of_1s = (np.log(predictions))*output
+            loss_of_0s = (np.log(1-predictions))*(1-output)
+            _loss = np.sum([loss_of_0s,loss_of_1s])
+            _loss = _loss/-n
+            for i in range(0,self.col_amount):
+                series_of_derivatives = series_of_difference*input.iloc[:,i]
+                total_gradient = (series_of_derivatives.sum())/n
+                step = total_gradient*learning_rate
+                new_weight = self.weights[i] - step
+                self.weights[i] = new_weight
+            total_gradient_bias = (series_of_difference.sum())/n
+            step = total_gradient_bias*learning_rate
+            self.bias = self.bias - step
+            return _loss
     def save(self):
          print("To save please note the returned values, and when creating a new linear regression object plug these values in as parameters, also pass the weights as a numpy array and if you want fit with your own params you can set useNewParams to False")
          print("weights: ",self.weights,"bias",self.bias)
          return {"weights":self.weights,"bias":self.bias}
-        
-df = pd.read_csv("/winequality-red.csv")
-df_train = df.iloc[0:math.floor(df.shape[0]*0.8)]
-df_test = df.iloc[math.floor(df.shape[0]*0.8):-1]
-X_train = df_train.iloc[:,0:-1]
-y_train = df_train.iloc[:,-1]
-X_test = df_test.iloc[:,0:-1]
-y_test = df_test.iloc[:,-1]
-my_model = LinearRegressionModel()
-my_model.fit(y=y_train,X=X_train, learning_rate=0.00001)
-prediction = my_model.predict(X_test.iloc[4])
-print("Prediction:",prediction,"Actual Value:",y_test.iloc[4])
-my_model.save()
+data = {
+    "feature1": [0.1, 1.2, -0.5, 0.3, -1.5, 0.7],
+    "feature2": [0.5, -0.3, 0.7, 1.0, -0.8, -0.2],
+    "target":   [0, 1, 0, 1, 0, 1]
+}
+df = pd.DataFrame(data=data)
+X = df.iloc[:,0:2]
+y = df.iloc[:, 2]
+my_model = SigmoidModel()
+my_model.fit(X=X, y=y, epochs=1000, learning_rate=60)
 plt.plot(epochs_plot,loss_plot)
 plt.show()
